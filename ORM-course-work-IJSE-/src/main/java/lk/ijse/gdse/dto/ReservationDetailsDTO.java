6a1529cead0dd1a2a96e235945c0085370f94dc8
Amila20 <amila7640@gmail.com>
2024-07-18 02:38:21 +0530

    Reservation Update

---
+++ ORM-course-work-IJSE-/src/main/java/lk/ijse/gdse/dto/ReservationDetailsDTO.java
@@ -0,0 +1,24 @@
+package lk.ijse.gdse.entity;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.sql.Date;
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Data
+@AllArgsConstructor
+@Entity
+public class ReservationDetails {
+    @Id
+    private String res_id;
+    private Date date;
+    private String student_id;
+    private String room_type_id;
+    private String status;
+
+
+}
